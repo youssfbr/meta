eefddb21bd52a68514e40735d9528f127c7e47ee
Alisson Youssf <youssfbr@gmail.com>
2022-07-16 13:04:31 -0300

    Frontend - first component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
-function App() {  
+import NotificationButton from "./components/NotificationButton"
 
+function App() {  
   return (
-    <h1>Olá Mundo</h1>
+    <>
+      <h1>Olá Mundo</h1>
+      <NotificationButton />    
+    </>
   )
 }
 
