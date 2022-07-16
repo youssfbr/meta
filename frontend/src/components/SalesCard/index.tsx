322db68b3c69fc303154d504b500e6c05f8c196f
Alisson Youssf <youssfbr@gmail.com>
2022-07-16 14:10:21 -0300

    Frontend - Datepicker

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,80 +1,89 @@
-import NotificationButton from '../NotificationButton';
-import './styles.css';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import NotificationButton from "../NotificationButton";
+import "./styles.css";
 
-function SalesCard() {  
-
+function SalesCard() {
   return (
     <div className="meta-card">
-            <h2 className="meta-sales-title">Vendas</h2>
-            <div>
-              <div className="meta-form-control-container">
-                <input className="meta-form-control" type="text" />
-              </div>
-              <div className="meta-form-control-container">
-                <input className="meta-form-control" type="text" />
-              </div>
-            </div>
+      <h2 className="meta-sales-title">Vendas</h2>
+      <div>
+        <div className="meta-form-control-container">
+          <DatePicker
+            selected={new Date()}
+            onChange={(date: Date) => {}}
+            className="meta-form-control"
+            dateFormat="dd/MM/yyyy"
+          />
+        </div>
+        <div className="meta-form-control-container">
+          <DatePicker
+            selected={new Date()}
+            onChange={(date: Date) => {}}
+            className="meta-form-control"
+            dateFormat="dd/MM/yyyy"
+          />
+        </div>
+      </div>
 
-            <div>
-              <table className="meta-sales-table">
-                <thead>
-                  <tr>
-                    <th className="show992">ID</th>
-                    <th className="show576">Data</th>
-                    <th>Vendedor</th>
-                    <th className="show992">Visitas</th>
-                    <th className="show992">Vendas</th>
-                    <th>Total</th>
-                    <th>Notificar</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td className="show992">#341</td>
-                    <td className="show576">08/07/2022</td>
-                    <td>Anakin</td>
-                    <td className="show992">15</td>
-                    <td className="show992">11</td>
-                    <td>R$ 55300.00</td>
-                    <td>
-                      <div className="meta-red-btn-container">
-                        <NotificationButton />
-                      </div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="show992">#341</td>
-                    <td className="show576">08/07/2022</td>
-                    <td>Anakin</td>
-                    <td className="show992">15</td>
-                    <td className="show992">11</td>
-                    <td>R$ 55300.00</td>
-                    <td>
-                      <div className="meta-red-btn-container">
-                        <NotificationButton />
-                      </div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="show992">#341</td>
-                    <td className="show576">08/07/2022</td>
-                    <td>Anakin</td>
-                    <td className="show992">15</td>
-                    <td className="show992">11</td>
-                    <td>R$ 55300.00</td>
-                    <td>
-                      <div className="meta-red-btn-container">
-                        <NotificationButton />
-                      </div>
-                    </td>
-                  </tr>
-                </tbody>
-
-              </table>
-            </div>
-
-          </div>
-  )
+      <div>
+        <table className="meta-sales-table">
+          <thead>
+            <tr>
+              <th className="show992">ID</th>
+              <th className="show576">Data</th>
+              <th>Vendedor</th>
+              <th className="show992">Visitas</th>
+              <th className="show992">Vendas</th>
+              <th>Total</th>
+              <th>Notificar</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="show992">#341</td>
+              <td className="show576">08/07/2022</td>
+              <td>Anakin</td>
+              <td className="show992">15</td>
+              <td className="show992">11</td>
+              <td>R$ 55300.00</td>
+              <td>
+                <div className="meta-red-btn-container">
+                  <NotificationButton />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td className="show992">#341</td>
+              <td className="show576">08/07/2022</td>
+              <td>Anakin</td>
+              <td className="show992">15</td>
+              <td className="show992">11</td>
+              <td>R$ 55300.00</td>
+              <td>
+                <div className="meta-red-btn-container">
+                  <NotificationButton />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td className="show992">#341</td>
+              <td className="show576">08/07/2022</td>
+              <td>Anakin</td>
+              <td className="show992">15</td>
+              <td className="show992">11</td>
+              <td>R$ 55300.00</td>
+              <td>
+                <div className="meta-red-btn-container">
+                  <NotificationButton />
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
-export default SalesCard
+export default SalesCard;
