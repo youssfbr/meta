41261e5111d8f6c7a66edbd40f7b22132241a0b2
Alisson Youssf <youssfbr@gmail.com>
2022-07-17 13:29:18 -0300

    Backend - Twilio SMS

--- backend/src/main/java/com/github/youssfbr/meta/controllers/SaleController.java
+++ backend/src/main/java/com/github/youssfbr/meta/controllers/SaleController.java
@@ -2,13 +2,11 @@ package com.github.youssfbr.meta.controllers;
 
 import com.github.youssfbr.meta.entities.Sale;
 import com.github.youssfbr.meta.services.ISaleService;
+import com.github.youssfbr.meta.services.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/sales")
@@ -16,6 +14,9 @@ public class SaleController {
 
     @Autowired
     private ISaleService saleService;
+
+    @Autowired
+    private SmsService smsService;
 
     @GetMapping
     public Page<Sale> findSales(
@@ -25,4 +26,10 @@ public class SaleController {
     ) {
         return saleService.findSales(minDate, maxDate, pageable);
     }
+
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id) {
+        smsService.sendSms(id);
+    }
+
 }
